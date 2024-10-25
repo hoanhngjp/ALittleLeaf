e480ef4ff64c2361bbe6e35d7ffff0f15c7fc61e
hoanhngjp <hoanhnghiep2704@gmail.com>
2024-10-25 16:42:15 +0700

    deleteVIewModelsFromModels

--- ALittleLeaf/Models/SearchViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ALittleLeaf.ViewModels
-{
-    public class SearchViewModel
-    {
-        public string Keyword { get; set; }
-        public List<string> SearchResults { get; set; } // Danh sách kết quả tìm kiếm
-    }
-
-}
