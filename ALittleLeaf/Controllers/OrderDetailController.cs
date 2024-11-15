61cacc72d30af30374fc9ba071ba19a4a90af19a
hoanhngjp <hoanhnghiep2704@gmail.com>
2024-11-15 12:22:00 +0700

    Finish OrderDetail

---
+++ ALittleLeaf/Controllers/OrderDetailController.cs
@@ -0,0 +1,38 @@
+﻿using ALittleLeaf.Models;
+using ALittleLeaf.Repository;
+using ALittleLeaf.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ALittleLeaf.Controllers
+{
+    public class OrderDetailController : Controller
+    {
+        private readonly AlittleLeafDecorContext _context;
+
+        public OrderDetailController(AlittleLeafDecorContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(int billId)
+        {
+            var billDetails = (from bd in _context.BillDetails
+                               join p in _context.Products on bd.IdProduct equals p.ProductId
+                               join pi in _context.ProductImages on p.ProductId equals pi.IdProduct
+                               where bd.IdBill == billId && pi.IsPrimary == true
+                               select new OrderDetailViewModel
+                               {
+                                   BillDetailId = bd.BillDetailId,
+                                   IdBill = bd.IdBill,
+                                   IdProduct = bd.IdProduct,
+                                   IdCategory = p.IdCategory,
+                                   Quantity = bd.Quantity,
+                                   UnitPrice = bd.UnitPrice,
+                                   TotalPrice = bd.TotalPrice,
+                                   ProductName = p.ProductName,
+                                   ProductImg = pi.ImgName // Chỉ lấy hình ảnh chính
+                               }).ToList();
+
+            return View(billDetails);
+        }
+    }
+}
---
+++ ALittleLeaf/ViewModels/OrderDetailViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ALittleLeaf.ViewModels
+{
+    public class OrderDetailViewModel
+    {
+        public int BillDetailId { get; set; }
+        public int IdBill { get; set; }
+        public int IdProduct { get; set; }
+        public int IdCategory { get; set; }
+        public string ProductName { get; set; }
+        public string ProductImg { get; set; }
+        public int Quantity { get; set; }
+        public int UnitPrice { get; set; }
+        public int TotalPrice { get; set; }
+    }
+}
