65eb34caf9b0a99f768dbce832fff97a2c7b08a9
hoanhngjp <hoanhnghiep2704@gmail.com>
2025-10-21 18:00:12 +0700

    change connectionString

--- ALittleLeaf/Repository/AlittleLeafDecorContext.cs
+++ ALittleLeaf/Repository/AlittleLeafDecorContext.cs
@@ -31,8 +31,11 @@ public partial class AlittleLeafDecorContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=NGJP;Initial Catalog=ALittleLeafDecor;Integrated Security=True;Trust Server Certificate=True");
+       
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
